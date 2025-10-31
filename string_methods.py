f0f84c8b76fc766ed5492290dcc376e21a50f34f
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2025-10-30 22:45:29 -0600

    keeping only boto3 python

--- string_methods.py
+++
@@ -1,9 +0,0 @@
-print("ha" *10)   #output hahahahahahahahahaha
-print("ha" * 0)  # prints nothing
-
-flowers= 2
-roses = input (" how many roses you want? ")
-
-bouquet= flowers * int(roses)
-
-print(bouquet)
